3b47ba58e7e82c9893ddb50ecb95786a6acf7077
Dave Reed <dave256@mac.com>
2021-01-30 21:10:40 -0500

    working version

--- NavigationDemo/ContentView.swift
+++ NavigationDemo/ContentView.swift
@@ -7,11 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct Item: Identifiable, Hashable {
+    var value: String
+    let id = UUID()
+
+    init(_ x: Int) {
+        self.value = String(format: "Item %02i", x)
     }
+}
+
+extension Item: Comparable {
+    static func < (lhs: Item, rhs: Item) -> Bool {
+        lhs.value < rhs.value
+    }
+}
+
+struct DetailView: View {
+    let item: Item
+
+    var body: some View {
+        Text(item.value)
+    }
+}
+
+struct ContentView: View {
+    @State private var items = (1...10).map { Item($0) }
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items, id: \.self) { item in
+                    NavigationLink(
+                        destination: DetailView(item: item),
+                        label: {
+                            Text(item.value)
+                        })
+                }
+                .onMove(perform: move)
+                .onDelete(perform: delete)
+            }
+            .navigationBarItems(leading: EditButton(), trailing: Button {
+                withAnimation() {
+                    items.append(Item(Int.random(in: 0...20)))
+                    items.sort()
+                }
+            } label: {
+                Image(systemName: "plus")
+            })
+            .navigationBarTitle("Navigation Demo", displayMode: .inline)
+            .listStyle(InsetGroupedListStyle())
+        }
+    }
+
+    func move(from source: IndexSet, to destination: Int) {
+        items.move(fromOffsets: source, toOffset: destination)
+    }
+
+    func delete(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
