884336509d12f685314b3db1fc964c89de632d10
Dave Reed <dave256@mac.com>
2021-01-30 20:02:58 -0500

    Initial Commit

---
+++ NavigationDemo/NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDemoApp.swift
+//  NavigationDemo
+//
+//  Created by David M Reed on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
